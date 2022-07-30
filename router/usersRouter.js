9b82f603e06fc7e97820ac9958eb8a6767c0da8f
Arijit Banarjee <ontikdev.2@gmail.com>
2022-07-30 15:39:12 +0600

    User crud Application

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,12 +1,35 @@
 //external imports
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
 // internal imports
-const { getUsers } = require("../controlles/usersController");
+const {
+  getUsers,
+  addUser,
+  removeUser,
+} = require("../controlles/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUploads = require("../middlewares/users/avatarUploads");
+
+const {
+  addUserValidator,
+  addUserValidationHandle,
+} = require("../middlewares/users/userValidator");
 
 // login page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add users
+router.post(
+  "/",
+  avatarUploads,
+  addUserValidator,
+  addUserValidationHandle,
+  addUser
+);
+
+// delete user
+router.delete("/:id", removeUser);
 
 module.exports = router;
